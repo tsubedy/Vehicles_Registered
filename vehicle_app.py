d5e2e2d565f9af7c99027217dbcf3202ae9b64b7
tsubedy <tsubedy@gmail.com>
2024-02-19 14:41:05 -0800

    Fixed syntax error

--- vehicle_app.py
+++ vehicle_app.py
@@ -33,14 +33,14 @@ US_Registered_Vehicles['year'] = pd.to_datetime(US_Registered_Vehicles['year'])
 
 # Getting the min and max date 
 startDate = pd.to_datetime(US_Registered_Vehicles['year']).min()
-endDate = pd.to_datetime(US_Registered_Vehicles['year'].max()
+endDate = pd.to_datetime(US_Registered_Vehicles['year']).max()
 
 with col1:
-    st.header("Seletion of Strating years")
+    st.subheader("Seletion of Strating years")
     date1 = pd.to_datetime(st.date_input("Start Year", startDate))
 
 with col2:
-    st.header("Seletion of Ending Year")
+    st.subheader("Seletion of Ending Year")
     date2 = pd.to_datetime(st.date_input("End Year", endDate))
 
 
